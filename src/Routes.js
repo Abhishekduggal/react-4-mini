8eabb4f775fcfa1bd503490dfbc85bf26da9b721
Abhishek Duggal <abhishekduggal@Abhisheks-MacBook-Pro.local>
2018-07-03 13:35:43 -0500

    Additions

--- src/Routes.js
+++ src/Routes.js
@@ -4,10 +4,17 @@ import Route1 from "./Route1";
 import Route2 from "./Route2";
 import Route3 from "./Route3";
 
-export default (
-  <Switch>
-    <Route exact path="/" component={Route1} />
-    <Route exact path="/2" component={Route2} />
-    <Route exact path="/3" component={Route3} />
-  </Switch>
-);
+// (React.Component is the same as {Component} in the header)
+
+export default class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Route1} />
+        <Route render={props => <Route2 products={this.props.products} />} />
+        <Route exact path="/Route3" component={Route3} />
+        {/* Route doesnt care about upper or lower cases */}
+      </Switch>
+    );
+  }
+}
